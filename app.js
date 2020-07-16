e2a8ed5b6289327a5732100043cafe7c2235060d
marierog <mroga18@freeuni.edu.ge>
2020-07-16 15:26:52 +0400

    add test front app

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require("express"); 
 var app = express();
+var fs = require("fs");
 // var bodyParser  = require("body-parser");
 
 app.use(express.static("public"));
 // app.set("view engine", "ejs");
 // app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+
+//test
+//app.use(express.static(__dirname + "/public"))  // !!ამის გარეშე რატომ მუშაობს!!
+app.get("/test", function(req, res){
+	fs.readFile('./public/test/test.html', function(err, data){
+		if (err) throw err;
+		var code = data.toString();
+		res.send(code);
+	});
+});
 
 app.get("/", function(req, res){
 	res.render("homepage.ejs");
