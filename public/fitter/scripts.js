6fb535509b3ceb350a4ee39de54f324f8c71940d
giochanturia <gchan13@freeuni.edu.ge>
2020-07-27 22:55:32 +0400

    Working on fitter.

--- public/fitter/scripts.js
+++ public/fitter/scripts.js
@@ -16,14 +16,96 @@ var formulas = [
     "$y = {\\color{red}a} x^{\\color{red}b}$"
 ]
 
+var parameters = [
+    "a", "b"
+]
+
+var parameterFixed = [
+    false, false
+]
+
+var order = parseInt(document.getElementById("order-input").value);
+
 function updateFormulas() {
-    var ddl = document.getElementById("inputGroupSelect01");
+    var ddl = document.getElementById("functionSelect");
     var selectedValue = parseInt(ddl.options[ddl.selectedIndex].value) - 1;
     document.getElementById("function-formula").innerHTML = formulas[selectedValue];
     MathJax.typeset();
+    if (selectedValue === 1) {
+        document.getElementById("order").style.display = "flex";
+    } else {
+        document.getElementById("order").style.display = "none";
+    }
     // MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'function-formula']);
     // MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
+
+function updateParameters() {
+    parameters = []
+    parameterFixed = []
+    var ddl = document.getElementById("functionSelect");
+    var selectedValue = parseInt(ddl.options[ddl.selectedIndex].value) - 1;
+    if (selectedValue === 1) {
+        for (var i = 0; i < order + 1; i++) {
+            parameters.push("a_{" + i + "}");
+            parameterFixed.push(false);
+        }
+    } else {
+        parameters.push("a");
+        parameters.push("b");
+        parameterFixed.push(false);
+        parameterFixed.push(false);
+    }
+    parametersHTML = "";
+    for (p of parameters) {
+        parametersHTML = parametersHTML + `
+        <div class="input-group mb-3" class="parameter-container">
+                <div class="input-group-prepend">
+                    <label class="input-group-text input-group-prepend">$${p}$</label>
+                    <div class="input-group-text" data-toggle="tooltip" data-placement="right" title="Parameter fixing not implemented yet.">
+                        <input type="checkbox" id="checkbox-${p}" class="input-group-text" onclick="toggleParameter('${p}')" checked disabled>
+                    </div>
+                </div>
+                <input type="number" step="0.01" class="form-control input-group-append" id="${p}" disabled>
+            </div>
+        `;
+    }
+    // console.log(parametersHTML);
+    document.getElementById("parameters-container").innerHTML = parametersHTML;
+    MathJax.typeset();
+    $('[data-toggle="tooltip"]').tooltip({
+        trigger: 'hover'
+    })
+}
+
+function toggleParameter(p) {
+    const i = parameters.indexOf(p);
+    if (i !== -1) {
+        const fixed = !document.getElementById("checkbox-" + p).checked;
+        parameterFixed[i] = fixed;
+        document.getElementById(p).disabled = fixed;
+    }
+}
+
+document.getElementById("order-input").addEventListener("input", (e) => {
+    if (document.getElementById("order-input").value === "") {
+        document.getElementById("order-input").value = 0;
+    }
+    else if (document.getElementById("order-input").value > 20) {
+        document.getElementById("order-input").value = 20;
+    } else if (document.getElementById("order-input").value < 0) {
+        document.getElementById("order-input").value = 0;
+    } else {
+        document.getElementById("order-input").value = parseInt(document.getElementById("order-input").value);
+    }
+    order = parseInt(document.getElementById("order-input").value);
+    updateParameters();
+});
+
+
+
+
+// SIDEBAR STUFF:
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -49,8 +131,9 @@ document.addEventListener("keyup", (e) => {
     if (e.keyCode === 83) { toggleNav(); }    // 's' on keyboard.
 });
 
-document.getElementById("inputGroupSelect01").addEventListener("change", (e) => {
+document.getElementById("functionSelect").addEventListener("change", (e) => {
     updateFormulas();
+    updateParameters();
 });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -58,39 +141,46 @@ document.getElementById("inputGroupSelect01").addEventListener("change", (e) => 
 // CANVAS:
 
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var scatterChart = new Chart(ctx, {
+    type: 'scatter',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+            label: 'Scatter Dataset',
+            backgroundColor: 'rgb(50,50,50)',
+            data: [
+                {x: 1,y: 4},
+                {x: 2,y: 5},
+                {x: 3,y: 6},
+                {x: 4,y: 7},
+                {x: 5,y: 8},
+                {x: 6,y: 9}
+            ]
+        },{
+            label: 'Line Dataset',
+            borderColor: 'rgb(255,50,50)',
+            backgroundColor: 'rgba(0,0,0,0)',
+            data: [
+                {x: 1,y: 4},
+                {x: 2,y: 5},
+                {x: 3,y: 6},
+                {x: 4,y: 7},
+                {x: 5,y: 8},
+                {x: 6,y: 9}
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            type: 'line'
         }]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
             }]
+        },
+        tooltips: {
+            filter: function (tooltipItem) {
+                return tooltipItem.datasetIndex === 0;
+            }
         }
     }
 });
